cd38b3337d6a2fc8430cc061ed3cfba998c764d0
hahand <hahand@yeah.net>
2021-10-27 10:46:21 +0800

    update: version 1.1.0

--- Jazzy/jazzy.sh
+++ Jazzy/jazzy.sh
@@ -1,2 +1,8 @@
 # 将swift和objc的文档合并成html
-jazzy --sourcekitten-sourcefile swiftDoc.json,objcDoc.json
+jazzy \
+ --clean \
+ --author hahand \
+ --author_url https://hahand.github.io \
+ --module-version 1.1.0 \
+ --theme apple \
+ --sourcekitten-sourcefile swiftDoc.json,objcDoc.json
